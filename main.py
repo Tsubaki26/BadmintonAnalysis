6c03589defcaa786c55cd15c705b3368a2d778e8
tsubaki <tsubaki.g3@gmail.com>
2024-11-11 16:04:21 +0900

    コードの整理

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class MatchAnalyzer:
         self.is_save_video = False
         self.writer = None
 
-        # プレイヤーの位置ポインターの初期設定
+        # player の位置ポインターの初期設定
         self.tracking_frames = 20
         self.point_radius = 8
         self.output_point_radius = 4
@@ -82,6 +82,11 @@ class MatchAnalyzer:
 
         # 動画中のコートの中心の高さ
         self.img_middle = 617
+
+        # 解析結果の出力用変数の初期設定
+        self.output_interval = self.tracking_frames
+        self.total_movement = [0, 0]
+        self.total_movement_track = [[0, 0]]
 
     def setup_video_writer(self, video_path, video_size, output_path):
         """
@@ -106,6 +111,18 @@ class MatchAnalyzer:
             .overwrite_output()
             .run_async(pipe_stdin=True)
         )
+
+    def write_frame(self, img):
+        """
+        Arguments:
+        ・BGR画像
+        """
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.writer.stdin.write(img.tobytes())
+
+    def finish_saving_video(self):
+        self.writer.stdin.close()
+        self.writer.wait()
 
     def make_point_size_list(self):
         """
@@ -138,7 +155,7 @@ class MatchAnalyzer:
         """
         playerの検出
 
-        Return:
+        Returns:
         ・選手達の足元の座標リスト
         ・選手の足元に楕円、頭上に名前が追加された画像
         """
@@ -219,22 +236,112 @@ class MatchAnalyzer:
                 )
         return player_foot_pos_xy_list, img_for_display
 
-    def get_perspective_trans_matrix(self, poslist1, poslist2):
+    def get_perspective_trans_matrix(self, poslist1):
         """
         射影変換行列を作成する。
-        Argument:
+        Arguments:
         ・変換前の4点座標（左上から右回り）
         ・変換後の4点座標（左上から右回り）
-        Return:
+        Returns:
         ・変換行列
         """
+        poslist2 = [
+            [0, 0],
+            [COURT_WIDTH, 0],
+            [COURT_WIDTH, COURT_HEIGHT],
+            [0, COURT_HEIGHT],
+        ]
         pts1 = np.float32(poslist1)
         pts2 = np.float32(poslist2)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         return M
 
+    def perspective_transform_position(self, player_foot_pos_xy_list, M):
+        pos_xy_perspective_transformed = []
+        for pos in player_foot_pos_xy_list:
+            # playerのポジション座標を射影変換
+            poslist_np = np.array(pos).reshape(-1, 1)
+            transformed_point = np.dot(M, poslist_np)
+            transformed_point = transformed_point / transformed_point[2]
+            pos_xy_perspective_transformed.append(transformed_point)
+
+    def plot_player_pos(self, court_img, pos_track, track_size_list):
+        for i, pos_xy_ in enumerate(pos_track):
+            for pos in pos_xy_:
+                x, y, _ = pos
+                x, y = int(x[0]), int(y[0])
+                # 過去に行くほど明るくなる
+                change_color = (self.tracking_frames - i - 1) * 5
+                cv2.circle(
+                    court_img,
+                    (x, y),
+                    track_size_list[i],
+                    color=(
+                        0 + change_color,
+                        125 + change_color,
+                        255 + change_color,
+                    ),
+                    thickness=-1,
+                )
+        return court_img
+
+    def add_total_movement(self, pos_track):
+        # 総移動距離の加算
+        move_amount_p1 = math.sqrt(
+            (pos_track[0][0][0] - pos_track[self.tracking_frames - 1][0][0]) ** 2
+            + (pos_track[0][0][1] - pos_track[self.tracking_frames - 1][0][1]) ** 2
+        )
+        move_amount_p2 = math.sqrt(
+            (pos_track[0][1][0] - pos_track[self.tracking_frames - 1][1][0]) ** 2
+            + (pos_track[0][1][1] - pos_track[self.tracking_frames - 1][1][1]) ** 2
+        )
+        self.total_movement[0] += move_amount_p1
+        self.total_movement[1] += move_amount_p2
+        self.total_movement_track.append(self.total_movement.copy())
+
+    def add_player_position(self, pos_xy_perspective_transformed):
+        circle_add = np.uint8(
+            np.zeros((self.court_size_wh[0], self.court_size_wh[1], 3))
+        )
+        for pos in pos_xy_perspective_transformed:
+            x, y, _ = pos
+            x, y = int(x[0]), int(y[0])
+            cv2.circle(
+                circle_add,
+                (x, y),
+                self.output_point_radius,
+                color=(50, 50, 50),
+                thickness=-1,
+            )
+            self.court_img_for_output = cv2.add(self.court_img_for_output, circle_add)
+
+    def make_total_movement_graph(self):
+        y1 = []
+        y2 = []
+        for ys in self.total_movement_track:
+            y1.append(ys[0])
+            y2.append(ys[1])
+        print(len(y1))
+        x = list(range(len(y1)))
+        plt.plot(x, y1, label="player1")
+        plt.plot(x, y2, label="player2")
+        plt.legend()
+        plt.xlabel("frame (1/15)")
+        plt.ylabel("total movement")
+        plt.savefig("./outputs/total_movement.jpg")
+        plt.show()
+        plt.close()
+
+    def make_player_position_plot(self):
+        # cv2.imshow("court", self.court_img_for_output)
+        # cv2.waitKey()
+        cv2.imwrite(f"./outputs/output{t}.jpg", self.court_img_for_output)
+
     def analyze_match(
-        self, match_video_path, is_save_video=False, output_path="./out.mp4"
+        self,
+        match_video_path,
+        is_save_video=False,
+        output_path="./out.mp4",
     ):
         """
         試合動画の解析
@@ -254,10 +361,6 @@ class MatchAnalyzer:
         pos_track = []
         track_size_list = self.make_point_size_list()
 
-        # 総移動距離のグラフ用配列
-        total_movement = [0, 0]
-        total_movement_track = [[0, 0]]
-
         while self.cap.isOpened():
             count += 1
 
@@ -275,145 +378,75 @@ class MatchAnalyzer:
             court_for_display = self.court_img.copy()
             img_for_display = frame.copy()
 
-            # プレイヤーの検出
+            #  player の検出
             player_foot_pos_xy_list, img_for_display = self.detect_player(
                 frame, img_for_display
             )
 
             """
-            射影変換
-            ・まだ変換前の座標設定は手動
+            player座標を射影変換し、コート画像上のplayerの位置を取得
             """
+            # 射影変換前の座標設定はいまのところ手動
             poslist1 = [
                 COURT_XY_BEFORE_WARP[0],
                 COURT_XY_BEFORE_WARP[1],
                 COURT_XY_BEFORE_WARP[2],
                 COURT_XY_BEFORE_WARP[3],
             ]
-            poslist2 = [
-                [0, 0],
-                [COURT_WIDTH, 0],
-                [COURT_WIDTH, COURT_HEIGHT],
-                [0, COURT_HEIGHT],
-            ]
-            M = self.get_perspective_trans_matrix(poslist1, poslist2)
-
+            M = self.get_perspective_trans_matrix(poslist1)
             # image = cv2.warpPerspective(frame, M, (COURT_WIDTH, COURT_HEIGHT))
-
-            """
-            コート画像上のplayerの位置を計算
-            """
-            pos_xy_warp = []
-            print(len(player_foot_pos_xy_list))
             # 両プレイヤーが検出できている時のみ記録。サイズが合わないから。いい方法が見つかれば変更する。
             if len(player_foot_pos_xy_list) >= 2:
-                for pos in player_foot_pos_xy_list:
-                    # playerのポジション座標を射影変換
-                    poslist_np = np.array(pos).reshape(-1, 1)
-                    transformed_point = np.dot(M, poslist_np)
-                    transformed_point = transformed_point / transformed_point[2]
-                    pos_xy_warp.append(transformed_point)
-
-                pos_track.append(pos_xy_warp)
+                pos_xy_perspective_transformed = self.perspective_transform_position(
+                    player_foot_pos_xy_list, M
+                )
+                pos_track.append(pos_xy_perspective_transformed)
                 # TRACK_FRAMESまでしか追跡しない
-                if len(pos_track) > TRACK_FRAMES:
+                if len(pos_track) > self.tracking_frames:
                     pos_track.pop(0)
 
             """
-            コート画像にプレイヤーの位置をプロット
+            コート画像に player の位置をプロット
             """
-            for i, pos_xy_warp_ in enumerate(pos_track):
-                for pos in pos_xy_warp_:
-                    x, y, _ = pos
-                    x, y = int(x[0]), int(y[0])
-                    # 過去に行くほど明るくなる
-                    change_color = (TRACK_FRAMES - i - 1) * 5
-                    cv2.circle(
-                        court_for_display,
-                        (x, y),
-                        track_size_list[i],
-                        color=(
-                            0 + change_color,
-                            125 + change_color,
-                            255 + change_color,
-                        ),
-                        thickness=-1,
-                    )
+            court_for_display = self.plot_player_pos(
+                court_for_display, pos_track, track_size_list
+            )
 
-            # 動画とコート画像の合成（左上）
+            """
+            動画とコート画像の合成（左上）
+            """
             img_for_display[50 : 50 + COURT_HEIGHT, 50 : 50 + COURT_WIDTH] = (
                 court_for_display
             )
 
             if self.is_save_video:
-                img_for_display = cv2.cvtColor(img_for_display, cv2.COLOR_BGR2RGB)
-                writer.stdin.write(img_for_display.tobytes())
+                self.write_frame(img_for_display)
             else:
+                """
+                フレームの表示
+                """
                 cv2.imshow("match", img_for_display)
                 # cv2.imshow("player1", player1_clip)
                 # cv2.imshow("player2", player2_clip)
                 cv2.waitKey()
 
             """
-            出力
-            ・総移動距離の加算
-            ・一定間隔でプレイヤーの位置をプロット
+            解析
             """
-            if count % OUTPUT_INTERVAL == 0 and count >= TRACK_FRAMES:
+            if count % self.output_interval == 0 and count >= self.tracking_frames:
                 # 総移動距離の加算
-                move_amount_p1 = math.sqrt(
-                    (pos_track[0][0][0] - pos_track[TRACK_FRAMES - 1][0][0]) ** 2
-                    + (pos_track[0][0][1] - pos_track[TRACK_FRAMES - 1][0][1]) ** 2
-                )
-                move_amount_p2 = math.sqrt(
-                    (pos_track[0][1][0] - pos_track[TRACK_FRAMES - 1][1][0]) ** 2
-                    + (pos_track[0][1][1] - pos_track[TRACK_FRAMES - 1][1][1]) ** 2
-                )
-                total_movement[0] += move_amount_p1
-                total_movement[1] += move_amount_p2
-                total_movement_track.append(total_movement.copy())
+                self.add_total_movement(pos_track)
 
-                # 一定間隔でプレイヤーの位置をプロット
-                circle_add = np.uint8(np.zeros((COURT_HEIGHT, COURT_WIDTH, 3)))
-                for pos in pos_xy_warp:
-                    x, y, _ = pos
-                    x, y = int(x[0]), int(y[0])
-                    cv2.circle(
-                        circle_add,
-                        (x, y),
-                        output_point_radius,
-                        color=(50, 50, 50),
-                        thickness=-1,
-                    )
-                    self.court_img_for_output = cv2.add(
-                        self.court_img_for_output, circle_add
-                    )
+                # player の位置を記録
+                self.add_player_position(pos_xy_perspective_transformed)
 
-        if SAVE_VIDEO:
-            writer.stdin.close()
-            writer.wait()
+        if self.is_save_video:
+            self.finish_saving_video()
 
         # 総移動距離のグラフ
-        y1 = []
-        y2 = []
-        for ys in total_movement_track:
-            y1.append(ys[0])
-            y2.append(ys[1])
-        print(len(y1))
-        x = list(range(len(y1)))
-        plt.plot(x, y1, label="player1")
-        plt.plot(x, y2, label="player2")
-        plt.legend()
-        plt.xlabel("frame (1/15)")
-        plt.ylabel("total movement")
-        plt.savefig("./outputs/total_movement.jpg")
-        plt.show()
-        plt.close()
-
-        # cv2.imshow("court", self.court_img_for_output)
-        # cv2.waitKey()
-
-        cv2.imwrite(f"./outputs/output{t}.jpg", self.court_img_for_output)
+        self.make_total_movement_graph()
+        # player の位置プロット
+        self.make_player_position_plot()
 
 
 if __name__ == "__main__":
